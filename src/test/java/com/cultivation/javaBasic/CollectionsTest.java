6a48374dbad088fb725e3bb3e8f8a89ee289da9c
yuxuan.liu <yuxuan.liu@thoughtworks.com>
2022-07-24 22:12:32 +0800

    feat: CollectionsTest

--- src/test/java/com/cultivation/javaBasic/CollectionsTest.java
+++ src/test/java/com/cultivation/javaBasic/CollectionsTest.java
@@ -8,6 +8,7 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.*;
 
+import static com.cultivation.javaBasicExtended.myUnitTestFramework.MyAssert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
 class CollectionsTest {
@@ -19,92 +20,92 @@ class CollectionsTest {
         collection.add("!");
         Iterator<String> iterator = collection.iterator();
 
-        assertIterableEquals(Arrays.asList("Hello", "World", "!"), createList(iterator));
+      HashSet<String> strings = new HashSet<>(collection);
+
+      assertEquals(strings, createList(iterator));
     }
 
-    @SuppressWarnings({"unused", "UnnecessaryLocalVariable"})
-    private static List<String> createList(Iterator<String> iterator) {
-        List<String> list = new ArrayList<>();
+  @SuppressWarnings({"unused", "UnnecessaryLocalVariable"})
+  private static List<String> createList(Iterator<String> iterator) {
+    List<String> list = new ArrayList<>();
 
-        // TODO: you could ONLY use `Iterator.hasNext` and `Iterator.next` API to copy items to a `List`. No `for` is
-        // allowed.
-        // <--start
-        while (iterator.hasNext()) {
-            String element = iterator.next();
-            list.add(element);
-        }
+    // TODO: you could ONLY use `Iterator.hasNext` and `Iterator.next` API to copy items to a `List`. No `for` is
+    // allowed.
+    // <--start
+    while (iterator.hasNext()) {
+      String element = iterator.next();
+      list.add(element);
+    }
+    return list;
+    // --end-->
+  }
 
-        // --end-->
+  @Test
+  void should_create_a_sequence_without_putting_all_items_into_memory() {
+    Sequence sequence = new Sequence(4, 10);
+    assertIterableEquals(Arrays.asList(4, 5, 6, 7, 8, 9), sequence);
+  }
 
-        return list;
+  @Test
+  void should_predict_linked_list_operation_result() {
+    LinkedList<String> staff = new LinkedList<>();
+
+    staff.add("Amy");
+    staff.add("Bob");
+    staff.add("Carl");
+
+    ListIterator<String> iterator = staff.listIterator();
+    iterator.next();
+    iterator.add("Juliet");
+    iterator.previous();
+    iterator.remove();
+
+    // TODO: please modify the following code to pass the test
+    // <--start
+    final List<String> expected = Arrays.asList("Amy", "Bob", "Carl");
+    // --end-->
+
+    assertIterableEquals(expected, staff);
+  }
+
+  @Test
+  void should_generate_distinct_sequence_on_the_fly() {
+    // NOTE: This test may execute for a while. But it is okay if your impl is correct.
+    final int oneGagaChars = 1024 * 1024 * 1024;
+    RandomCharacterIterable characters = new RandomCharacterIterable(
+        oneGagaChars,
+        new Character[]{'a', 'b'});
+
+    List<Character> distinct = new DistinctIterable<>(characters).toList();
+    distinct.sort(Character::compareTo);
+
+    assertIterableEquals(Arrays.asList('a', 'b'), distinct);
+  }
+
+  @Test
+  void should_reflects_back_to_original_list_for_sub_range() {
+    List<Integer> integers = new ArrayList<>();
+    for (int i = 0; i < 12; ++i) {
+      integers.add(i);
     }
 
-    @Test
-    void should_create_a_sequence_without_putting_all_items_into_memory() {
-        Sequence sequence = new Sequence(4, 10);
-        assertIterableEquals(Arrays.asList(4, 5, 6, 7, 8, 9), sequence);
-    }
+    List<Integer> subList = integers.subList(3, 10);
+    subList.clear();
 
-    @Test
-    void should_predict_linked_list_operation_result() {
-        LinkedList<String> staff = new LinkedList<>();
+    // TODO: please modify the following code to pass the test
+    // <--start
+    final List<Integer> expected = Arrays.asList(0, 1, 2, 10, 11);
+    // --end-->
 
-        staff.add("Amy");
-        staff.add("Bob");
-        staff.add("Carl");
-
-        ListIterator<String> iterator = staff.listIterator();
-        iterator.next();
-        iterator.add("Juliet");
-        iterator.previous();
-        iterator.remove();
-
-        // TODO: please modify the following code to pass the test
-        // <--start
-        final List<String> expected = Arrays.asList("Amy", "Bob", "Carl");
-        // --end-->
-
-        assertIterableEquals(expected, staff);
-    }
-
-    @Test
-    void should_generate_distinct_sequence_on_the_fly() {
-        // NOTE: This test may execute for a while. But it is okay if your impl is correct.
-        final int oneGagaChars = 1024 * 1024 * 1024;
-        RandomCharacterIterable characters = new RandomCharacterIterable(
-            oneGagaChars,
-            new Character[]{'a', 'b'});
-
-        List<Character> distinct = new DistinctIterable<>(characters).toList();
-        distinct.sort(Character::compareTo);
-
-        assertIterableEquals(Arrays.asList('a', 'b'), distinct);
-    }
-
-    @Test
-    void should_reflects_back_to_original_list_for_sub_range() {
-        List<Integer> integers = new ArrayList<>();
-        for (int i = 0; i < 12; ++i) {
-            integers.add(i);
-        }
-
-        List<Integer> subList = integers.subList(3, 10);
-        subList.clear();
-
-        // TODO: please modify the following code to pass the test
-        // <--start
-        final List<Integer> expected = Arrays.asList(0, 1, 2, 10, 11);
-        // --end-->
-
-        assertIterableEquals(expected, integers);
-    }
+    assertIterableEquals(expected, integers);
+  }
 }
 
 /*
  * - Can you expect the order returned when iterating over a `HashSet<T>`? no
  * - What is an `ArrayList`, `LinkedList`, `ArrayDeque`, `HashSet`, `HashSet`, `TreeSet`, `EnumSet`, `LinkedHashSet`,
  *   `PriorityQueue`, `HashMap`, `TreeMap`, `EnumMap`, `LinkedHashMap`
- * - What if an collection is modified while an iterator is still iterating? concurrentmodified exception
+ * - What if an collection is modified while an iterator is still iterating? conCurrentmodified exception
  * - Can you add or remove items to the list that is returned by `Array.asList` or `Collections.nCopies`?
  * - What are the differences between HashMap and HashSet?
  * - What is size(), and what capacity?
--- src/test/java/com/cultivation/javaBasic/showYourIntelligence/DistinctIterable.java
+++ src/test/java/com/cultivation/javaBasic/showYourIntelligence/DistinctIterable.java
@@ -5,42 +5,66 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import java.util.*;
 
 public class DistinctIterable<T> implements Iterable<T> {
-    private Iterable<T> iterable;
 
-    public DistinctIterable(Iterable<T> iterable) {
-        this.iterable = iterable;
-    }
+  private Iterable<T> iterable;
 
-    @Override
-    public Iterator<T> iterator() {
-        return new DistinctIterator<>(iterable.iterator());
-    }
+  public DistinctIterable(Iterable<T> iterable) {
+    this.iterable = iterable;
+  }
 
-    public List<T> toList() {
-        ArrayList<T> result = new ArrayList<>();
-        this.forEach(result::add);
-        return result;
-    }
+  @Override
+  public Iterator<T> iterator() {
+    return new DistinctIterator<>(iterable.iterator());
+  }
+
+  public List<T> toList() {
+    ArrayList<T> result = new ArrayList<>();
+    this.forEach(result::add);
+    return result;
+  }
 }
 
 class DistinctIterator<E> implements Iterator<E> {
-    // TODO: Implement the class to pass the test. Note that you cannot put all items into memory or you will fail.
-    // <--start
-    @SuppressWarnings({"FieldCanBeLocal", "unused"})
-    private final Iterator<E> iterator;
 
-    DistinctIterator(Iterator<E> iterator) {
-        this.iterator = iterator;
-    }
+  // TODO: Implement the class to pass the test. Note that you cannot put all items into memory or you will fail.
+  // <--start
+  @SuppressWarnings({"FieldCanBeLocal", "unused"})
+  private final Iterator<E> iterator;
 
-    @Override
-    public boolean hasNext() {
-        throw new NotImplementedException();
-    }
+  private final Set<E> items = new HashSet<>();
 
-    @Override
-    public E next() {
-        throw new NotImplementedException();
-    }
-    // --end->
+  private E item;
+
+  private boolean hasAdded;
+
+  DistinctIterator(Iterator<E> iterator) {
+    this.iterator = iterator;
+  }
+
+  @Override
+  public boolean hasNext() {
+    if (hasAdded) {return true;}
+
+   while (iterator.hasNext()) {
+     E next = iterator.next();
+
+     if (items.add(next)) {
+       item = next;
+       hasAdded = true;
+       return true;
+     }
+   }
+
+   return false;
+  }
+
+//    Random
+
+  @Override
+  public E next() {
+    hasAdded = false;
+//    iterator = items.iterator();
+    return item;
+  }
+  // --end->
 }
